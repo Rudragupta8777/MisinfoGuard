e2148319fa9b59f5e9b2263864d201af2c8e477a
Rudragupta8777 <rudragupta1204@gmail.com>
2025-02-25 21:28:56 +0530

    check credientials

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-// import { firebaseConfig } from "./config.js"; // Import credentials from config.js
-
-const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
-};
+import { firebaseConfig } from "./config.js"; // Import credentials from config.js
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
