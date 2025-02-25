b1ff0c920c332513223b98a4e39a54c883c7edc8
Rudragupta8777 <rudragupta1204@gmail.com>
2025-02-25 22:06:29 +0530

    check credientials

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "VITE_FIREBASE_API_KEY",
-    authDomain: "VITE_FIREBASE_AUTH_DOMAIN",
-    projectId: "VITE_FIREBASE_PROJECT_ID",
-    storageBucket: "VITE_FIREBASE_STORAGE_BUCKET",
-    messagingSenderId: "VITE_FIREBASE_MESSAGING_SENDER_ID",
-    appId: "VITE_FIREBASE_APP_ID"
+    apiKey: "AIzaSyD-2byLrculA0a9yNj9Q8aTgalfEP4C8c0",
+    authDomain: "misinfoguard.firebaseapp.com",
+    projectId: "misinfoguard",
+    storageBucket: "misinfoguard.firebasestorage.app",
+    messagingSenderId: "449109220623",
+    appId: "1:449109220623:web:da6aab308ef0ecdb9d727a"
 };
--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-import { firebaseConfig } from "./config.js"; // Import credentials from config.js
+// import { firebaseConfig } from "./config.js"; // Import credentials from config.js
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
+// firebase.initializeApp(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
